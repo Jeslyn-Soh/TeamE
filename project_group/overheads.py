e7b6c3fdff7de996b659476461f7f70d05d7c12f
Jeslyn-Soh <s10242009@connect.np.edu.sg>
2022-08-02 16:16:59 +0800

    almost finished part a

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -1,27 +1,29 @@
 from pathlib import Path
-import csv
+import csv, re
 
 home = Path.cwd()
 file_path=home/"project_group"/"csv_reports"
-print(file_path.exists())
+#print(file_path.exists())
 
-for file in file_path.glob("*.csv"):
-    print(file)
+#for file in file_path.glob("*.csv"):
+    #print(file)
 
 OH = home/"project_group"/"csv_reports"/"Overheads.csv"
 with OH.open(mode="r",encoding="UTF-8") as file:
     text=file.read()
+    print(text)
 
 fp_txt = home/"project_group"/"summary_report.txt"
 fp_txt.touch()
 print(fp_txt.exists())
 
+empty_list = []
+
 with fp_txt.open(mode = "w", encoding="UTF-8", newline="") as file:
     writer = csv.writer(file)
-    def highest_OH(Category, Overheads):
-        return f"[HIGHEST OVERHEADS {max(Category)}: SGD{max(Overheads)}]"
-print(highest_OH)
-
-def highest_OH(Category, Overheads):
-    return f"[HIGHEST OVERHEADS {Category}: SGD{Overheads}]"
-print(max(text))
+    max_OH = max(re.findall(pattern = ("[0-9].+.[0-9].+"), string = text))
+    ans = f"[HIGHEST OVERHEADS {max_OH}: SGD{max_OH}]"
+    for info in max_OH:
+        empty_list.append(ans)
+        break
+    writer.writerow(empty_list)
