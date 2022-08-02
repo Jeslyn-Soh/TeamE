2629bcaf1586958785bc8f3dbd3e13623981000d
weiqi79 <s10243981@connect.np.edu.sg>
2022-08-02 16:19:21 +0800

    bb

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -1,27 +1,32 @@
 from pathlib import Path
 import csv
+import re
 
 home = Path.cwd()
 file_path=home/"project_group"/"csv_reports"
 print(file_path.exists())
 
-for file in file_path.glob("*.csv"):
-    print(file)
 
 OH = home/"project_group"/"csv_reports"/"Overheads.csv"
 with OH.open(mode="r",encoding="UTF-8") as file:
     text=file.read()
+print(text)
+
 
 fp_txt = home/"project_group"/"summary_report.txt"
 fp_txt.touch()
 print(fp_txt.exists())
+empty_list=[]
 
 with fp_txt.open(mode = "w", encoding="UTF-8", newline="") as file:
     writer = csv.writer(file)
-    def highest_OH(Category, Overheads):
-        return f"[HIGHEST OVERHEADS {max(Category)}: SGD{max(Overheads)}]"
-print(highest_OH)
+    empty_list=[]
+    max1=max(re.findall(pattern="[0-9][0-9].+",string=text))
+    cat1=max(re.findall(pattern="[A-Z].+,[0-9][0-9].+",string=text))
+    maxt = f"[HIGHEST OVERHEADS] {cat1} SGD:{max1}"
+    for ans in text :
+        ans=empty_list.append(maxt)
+        break
+    writer.writerow(empty_list)
 
-def highest_OH(Category, Overheads):
-    return f"[HIGHEST OVERHEADS {Category}: SGD{Overheads}]"
-print(max(text))
+
