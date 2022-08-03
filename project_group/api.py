4f75584fea849a4fb294b3566697294ac965fba2
Jeslyn-Soh <s10242009@connect.np.edu.sg>
2022-08-03 17:30:33 +0800

    finished api and trying profit_loss

--- project_group/api.py
+++ project_group/api.py
@@ -1,8 +1,33 @@
 api_key = "IUMVK4SLEPVSK1MW"
 
+from xml.dom import UserDataHandler
 import requests
 
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=IUMVK4SLEPVSK1MW'
 
 response = requests.get(url)
-print(response.json())
+final_response = response.json()
+print(final_response)
+
+from pathlib import Path
+import  re
+
+home = Path.cwd()
+file_path=home/"project_group"/"csv_reports"
+
+fp_txt = home/"project_group"/"summary_report.txt"
+fp_txt.touch()
+
+#function = "FX_WEEKLY"
+#from_currency = "USD"
+#to_currency = "SGD"
+
+with fp_txt.open(mode = "w") as file:
+    USD = re.findall(pattern = ["1. From_Currency Code.+"], string = final_response)
+    SGD = re.findall(pattern = ["3. To_Currency Code.+"], string = final_response)
+    rate = (final_response["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+    ans_rate = f"[REAL TIME CURRENCY CONVERSION RATE]  = {SGD}{rate}"
+    for info in url:
+        file.write(ans_rate)
+        break
+
--- project_group/overheads.py
+++ project_group/overheads.py
@@ -7,7 +7,7 @@ file_path=home/"project_group"/"csv_reports"
 OH = home/"project_group"/"csv_reports"/"Overheads.csv"
 with OH.open(mode="r",encoding="UTF-8") as file:
     text=file.read()
-print(text)
+    print(text)
 
 fp_txt = home/"project_group"/"summary_report.txt"
 fp_txt.touch()
@@ -27,11 +27,26 @@ with fp_txt.open(mode = "w", encoding="UTF-8", newline="") as file:
     writer.writerow(empty_list)
 
 #JESLYN
+api_key = "IUMVK4SLEPVSK1MW"
+
+import requests
+
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=IUMVK4SLEPVSK1MW'
+
+response = requests.get(url)
+final_response = response.json()
+
+
 with fp_txt.open(mode = "w", encoding="UTF-8", newline="") as file:
     writer = csv.writer(file)
-    max_OH = max(re.findall(pattern = ("[0-9].+.[0-9].+"), string = text))
-    ans = f"[HIGHEST OVERHEADS {max_OH}: SGD{max_OH}]"
-    for info in max_OH:
+    rate = (final_response["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+with fp_txt.open(mode = "w", encoding="UTF-8", newline="") as file:
+    writer = csv.writer(file)
+    max_OH = max(re.findall(pattern = "[0-9].+.[0-9].+", string = text))
+    cat = max(re.findall(pattern = "[A-Z].+Expense", string = text))
+    ans = f"[HIGHEST OVERHEADS] {cat}: SGD{float(max_OH) * float(rate)}"
+    for info in ans:
         empty_list.append(ans)
         break
     writer.writerow(empty_list)
