f5239f2987d4721431f86bf88512b418593f786f
yuxuan999 <s10243024@connect.np.edu.sg>
2022-08-04 21:38:40 +0800

    cash on hand

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -7,10 +7,14 @@ home = Path.cwd()
 file_path=home/"project_group"/"csv_reports"
 
 COH = home/"project_group"/"csv_reports"/"Cash On Hand.csv"
-with COH.open(mode="r",encoding="UTF-8")as file :
-    text = file.read()
-    print(text)
+#with COH.open(mode="r",encoding="UTF-8")as file :
+#    text = file.read()
+#    print(text("Day"))
 
+with COH.open(mode="r",encoding="UTF-8") as file:
+    COHreader = csv.DictReader(file)
+    for row in COHreader:
+        print(row["Cash On Hand"])
 
 
 
