704d4d39405fc95c2d05256c213e9db3a276258b
weiqi79 <s10243981@connect.np.edu.sg>
2022-08-04 23:08:06 +0800

    last

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -1,10 +1,45 @@
+import csv
+with open("pnl.csv",'rt')as f:
+    data = csv.reader(f)
+    prev_day = 0
+    prev_profit = 0
+    counter = 0
+    for row in data:
+        if counter >0:
+            day = row[0]
+            net_profit = row[4]
+            if counter > 1:
+                if int(prev_profit) > int(net_profit):
+                    profit_diff = int(prev_profit) - int(net_profit)
+                    print("The Net profit difference between day",prev_day,"and",day,"is $",profit_diff)
+            prev_day = day
+            prev_profit = net_profit
+        counter += 1
+
 from pathlib import Path
 import csv, re
 
-home = Path.cwd()
-file_path=home/"project_group"/"csv_reports"
+import csv
 
-PL = home/"project_group"/"csv_reports"/"Profits and Loss.csv"
+with open("pnl.csv",'rt')as f:
+    #home = Path.cwd()
+    #file_path=home/"project_group"/"pnl.csv"
+    #PL = home/"project_group"/"pnl.csv"
+    data = csv.reader(f)
+    prev_day = 0
+    prev_profit = 0
+    counter = 0
+    for row in data:
+        if counter >0:
+            day = row[0]
+            net_profit = row[4]
+            if counter > 1:
+                if int(prev_profit) > int(net_profit):
+                    profit_diff = int(prev_profit) - int(net_profit)
+                    print("The Net profit difference between day",prev_day,"and",day,"is $",profit_diff)
+            prev_day = day
+            prev_profit = net_profit
+        counter += 1
 
 with PL.open(mode="r",encoding="UTF-8") as file:
     PLreader = csv.DictReader(file)
