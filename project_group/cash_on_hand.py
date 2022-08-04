8873502c271aaa3adb7f862cf21b8fb004d5ba62
yuxuan999 <s10243024@connect.np.edu.sg>
2022-08-04 21:02:36 +0800

    nth

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -9,7 +9,5 @@ file_path=home/"project_group"/"csv_reports"
 COH = home/"project_group"/"csv_reports"/"Cash On Hand.csv"
 with COH.open(mode="r",encoding="UTF-8")as file :
     text = file.read()
-print(text)
-
 
 
