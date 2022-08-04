1848c5313f0290bf1f41baa8e705e6039a76eba8
yuxuan999 <s10243024@connect.np.edu.sg>
2022-08-04 20:16:27 +0800

    format

--- project_group/api.py
+++ project_group/api.py
@@ -22,7 +22,7 @@ file_path = home/"project_group"/"csv_reports"
 fp_txt = home/"project_group"/"summary_report.txt"
 
 
-with fp_txt.open(mode = "w") as file:
+with fp_txt.open(mode = "w",encoding="UTF-8", newline="") as file:
     #open the file in write mode to place the data of the exchange rate inside
     rate = (final_response["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
     #'rate' is a variable assigned to find the exchange rate from a list in another list
