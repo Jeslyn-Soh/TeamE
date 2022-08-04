724658dff1fd533c21277155f3628e8f955520fb
Jeslyn-Soh <s10242009@connect.np.edu.sg>
2022-08-04 18:00:50 +0800

    finished api without comments

--- project_group/api.py
+++ project_group/api.py
@@ -10,23 +10,16 @@ final_response = response.json()
 print(final_response)
 
 from pathlib import Path
-import  re
 
 home = Path.cwd()
-file_path=home/"project_group"/"csv_reports"
+file_path = home/"project_group"/"csv_reports"
 
 fp_txt = home/"project_group"/"summary_report.txt"
 fp_txt.touch()
 
-#function = "FX_WEEKLY"
-#from_currency = "USD"
-#to_currency = "SGD"
-
 with fp_txt.open(mode = "w") as file:
-    USD = re.findall(pattern = ["1. From_Currency Code.+"], string = final_response)
-    SGD = re.findall(pattern = ["3. To_Currency Code.+"], string = final_response)
     rate = (final_response["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
-    ans_rate = f"[REAL TIME CURRENCY CONVERSION RATE]  = {SGD}{rate}"
+    ans_rate = f"[REAL TIME CURRENCY CONVERSION RATE]  = SGD{rate}"
     for info in url:
         file.write(ans_rate)
         break
