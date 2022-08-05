904f1657c73cf25a920b12bd992566e423a8dda0
Jeslyn-Soh <s10242009@connect.np.edu.sg>
2022-08-05 17:11:14 +0800

    coh

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,23 +1,27 @@
 from pathlib import Path
 import csv
-import re
-
 
 home = Path.cwd()
 file_path=home/"project_group"/"csv_reports"
 
 COH = home/"project_group"/"csv_reports"/"Cash On Hand.csv"
-#with COH.open(mode="r",encoding="UTF-8")as file :
-#    text = file.read()
-#    print(text("Day"))
-empty_list = []
+  
 with COH.open(mode="r",encoding="UTF-8") as file:
-    COHreader = csv.DictReader(file)
-    for row in COHreader:
-        list = ([row["Cash On Hand"]])
-        
-
-
-
-
-
+    data = csv.reader(file)
+    prev_day = 0
+    prev_coh = 0
+    counter = 0
+    for row in data:
+        if counter > 1:
+            day = row[0]
+            coh = row[1]
+            if counter > 1:
+                if int(prev_coh) > int(coh):
+                    COH_diff = int(prev_coh) - int(coh)
+                    print(f"[CASH DEFICIT] DAY: {day} AMOUNT: SGD{COH_diff}")
+                else:
+                    print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+                    #only need to come out if all days coh is higher than the on before
+            prev_day = day
+            prev_coh = coh
+        counter += 1
