5739e4393f6ccee6cc28e4242e278169198c34c3
W7 <110237439+weiqi79@users.noreply.github.com>
2022-08-05 21:19:11 +0800

    Update profit_loss.py

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -33,12 +33,12 @@ fp_txt = home/"project_group"/"summary_report.txt"
 fp_txt.touch()
 #create a new txt file named "summary_report" by using 'touch' method
 
-class summary4:
-    
- with fp_txt.open(mode = "r",encoding="UTF-8", newline="") as file:
-    #open the file in read mode to access the data inside
+with fp_txt.open(mode = "r",encoding="UTF-8", newline="") as file:
+#open the file in read mode to access the data inside
     rate = (final_response["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
     #using the function to exchange the currency,assigned it as rate
+
+
     with PNL.open(mode="r",encoding="UTF-8") as file:
         data = csv.reader(file)
         prev_day = 0
@@ -51,13 +51,26 @@ class summary4:
                 if counter > 1:
                     if int(prev_np) > int(np):
                         np_diff = int(prev_np) - int(np)
-                        fnp_diff = (np_diff*float(rate) *-1)
+                        fnp_diff = f"[NET PROFIT DEFICIT] DAY: {day} AMOUNT: SGD{(np_diff*float(rate) *-1)}"
         #using csv.reader to read the file then by using the function to find the NET PROFIT DEFICIT
-                        print(f"[NET PROFIT DEFICIT] DAY: {day} AMOUNT: SGD{fnp_diff}")
+                        
+                        print(fnp_diff)
                     else:
-                        print("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+                        nps = "[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+                        print(nps)
                         break
                         #only need to come out one time if all days np is higher than the on before
                 prev_day = day
                 prev_np = np
-            counter += 1
+            counter += 1 
+
+class summary3:
+#assign the function as summary3 for modularisation in main.py
+
+ empty_list = []
+
+ with fp_txt.open(mode="w",encoding="UTF-8",newline="") as file :
+      writer=csv.writer(file)
+      empty_list.append(nps)
+      writer.writerow(empty_list)
+ #wrtting 'nps' into summary_report by  using 'w' mode
